8d70cb54ae37deaf8eb79f18415a9caa392b28b6
Suraj-Umarani <01fe22bca170@kletech.ac.in>
2024-02-20 10:14:55 +0530

    Code for Extended Calculator

---
+++ ExtendedCalculator.java
@@ -0,0 +1,11 @@
+public class ExtendedCalculator {
+    // Returns the square of a number
+    public float square(float operand_1) {
+        return (operand_1 * operand_1);
+    }
+
+    // Returns the cube of a number
+    public float cube(float operand_1) {
+        return (operand_1 * operand_1 * operand_1);
+    }
+}
